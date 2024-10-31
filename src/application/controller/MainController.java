6ad394dce9e0532319958d4816d15bf6a2db2765
kingsand <clinvil2@gmail.com>
2024-10-31 16:50:26 -0700

    Now swtiches colors based on section, added section headers, and added scroll bar

--- src/application/controller/MainController.java
+++ src/application/controller/MainController.java
@@ -1,22 +1,32 @@
 package application.controller;
 
-import java.io.IOException;
 import java.net.URL;
+import java.io.IOException;
 
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.LinearGradient;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.Stop;
+
 
 public class MainController {
 
 	@FXML HBox mainBox;
-	
-	@FXML
+
+    @FXML private AnchorPane sidebar;
+
+    @FXML
 	public void initialize() {
 		// Show the homepage on startup
 		switchToHome();
-	}
+    }
 	
 	//loads the given URL, and replaces the 2nd child of main/ the content to the right of the sidebar
     private void loadScene(String fxmlFile) {
@@ -25,12 +35,12 @@ public class MainController {
 
         try {
 			AnchorPane paneHome = (AnchorPane)FXMLLoader.load(url);
-			
+
 			if (mainBox.getChildren().size() >1)
 				mainBox.getChildren().remove(1);
 			
 			mainBox.getChildren().add(paneHome);
-			
+
 		} catch (IOException e) {
 			System.out.println("erorr loading scene from " + fxmlFile);
 			e.printStackTrace();
@@ -41,29 +51,32 @@ public class MainController {
     @FXML
     public void switchToHome() {
         loadScene("/view/homepage.fxml"); // Use the loadScene method
+        sidebar.getStyleClass().setAll("default-sidebar"); // Apply default gradient
     }
 
     // Switches to the "New Account" screen
     @FXML
     public void switchToNewAccount() {
+        sidebar.getStyleClass().setAll("red-sidebar"); // Apply red gradient
         loadScene("/view/newAccount.fxml"); // Use the loadScene method
     }
     
     // Switches to the "View Accounts" screen
     @FXML
     public void switchToViewAccounts() {
+        sidebar.getStyleClass().setAll("green-sidebar"); // Apply green gradient
         loadScene("/view/viewAccounts.fxml"); // Use the loadScene method
     }
     // Switches to the "New Transaction" screen
     @FXML
     public void switchToNewTransaction() {
+        sidebar.getStyleClass().setAll("red-sidebar"); // Apply red gradient
         loadScene("/view/newTransaction.fxml"); // Use the loadScene method
     }
  // Switches to the "New Transaction Type" screen
     @FXML
     public void switchToNewTransactionType() {
+        sidebar.getStyleClass().setAll("red-sidebar"); // Apply red gradient
         loadScene("/view/newTransactionType.fxml"); // Use the loadScene method
     }
-
-
 }
